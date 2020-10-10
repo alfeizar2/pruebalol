f97497f27d4470b1a95f106f6ebbe9950e872f9e
alfeizar2 <72641296+alfeizar2@users.noreply.github.com>
2020-10-10 00:08:05 -0500

    lol
    lol

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.set('port', process.env.PORT || 5000)
 app.listen(app.get('port'), () => {
     console.log('servidor escuchando por el puerto ', app.get('port'))
     setInterval(async () => {
+        await Axios.get('https://sepia-regular-cheshire.glitch.me/')
+        console.log('peticion propia')
         await Axios.get('https://chain-scalloped-brain.glitch.me/list')
         console.log('peticion hecha al backend de imagenes')
         await Axios.get('https://jhoan12-backend-portafolio-1.glitch.me/usuario/usuarios')
